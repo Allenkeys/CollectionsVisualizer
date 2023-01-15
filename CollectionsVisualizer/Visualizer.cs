75e52f2321823f51e992726118264a510626959d
Joshua Eze <ezejoshuac@gmail.com>
2023-01-15 23:59:39 +0100

    Add project files.

---
+++ CollectionsVisualizer/Visualizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsVisualizer
+{
+    public class Visualizer
+    {
+        public static void Display(string[] strArr)
+        {
+            foreach (var item in strArr)
+            {
+                Console.Write($"[{item},]");
+            }
+        }
+
+        public static void Display(List<string> strArr)
+        {
+            foreach (var item in strArr)
+            {
+                Console.Write($"[{item},]");
+            }
+        }
+
+    }
+}
