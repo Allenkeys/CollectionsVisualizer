98750478f1c98f846284b2d8ebb0e3719dca49a1
Joshua Eze <ezejoshuac@gmail.com>
2023-01-16 14:55:08 +0100

    Applied testing for the package

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using CollectionsVisualizer;
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> strings = new List<string> { "Josh", "Amara", "Mum" };
+            string[] str = { "Test", "eggs", "computer" };
+            int[] nums = { 1, 2, 3 };
+            Visualizer.Display(str);
+            Visualizer.Display(strings);
+            Visualizer.Display(nums);
+        }
+    }
+}
